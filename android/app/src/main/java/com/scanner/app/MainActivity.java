27d05f6f330e8a9b2795880d803a70d6a6f1bcae
n4me12-creator <n4me12@gmail.com>
2026-05-19 08:37:15 +0300

    Add files via upload

--- android/app/src/main/java/com/scanner/app/MainActivity.java
+++ android/app/src/main/java/com/scanner/app/MainActivity.java
@@ -1,14 +1,23 @@
 package com.scanner.app;
 
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // اجعل WebView شفافًا حتى تظهر الكاميرا خلفه
-        getBridge().getWebView().setBackgroundColor(Color.TRANSPARENT);
+        // اجعل WebView شفافًا حتى تظهر الكاميرا خلفه عند استخدام CameraPreview
+        WebView webView = getBridge().getWebView();
+        if (webView != null) {
+            webView.setBackgroundColor(Color.TRANSPARENT);
+            webView.setBackground(new ColorDrawable(Color.TRANSPARENT));
+            // اجعل WebView يرسم فوق محتوى آخر
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        }
     }
 }
