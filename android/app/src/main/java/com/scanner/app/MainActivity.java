9cf84c1928c9bce1233a25af4ca9d2ec5c0b88e5
n4me12-creator <n4me12@gmail.com>
2026-05-19 07:45:17 +0300

    Override onCreate to set WebView background color
    Make WebView transparent to show the camera behind it.

--- android/app/src/main/java/com/scanner/app/MainActivity.java
+++ android/app/src/main/java/com/scanner/app/MainActivity.java
@@ -1,5 +1,14 @@
 package com.scanner.app;
 
+import android.graphics.Color;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // اجعل WebView شفافًا حتى تظهر الكاميرا خلفه
+        getBridge().getWebView().setBackgroundColor(Color.TRANSPARENT);
+    }
+}
